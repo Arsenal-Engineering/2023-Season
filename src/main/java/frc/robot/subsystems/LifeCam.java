58820fc6efa29b58bfeb9dfdc1def80ea7281ad6
ArsenalEngineering <fallsrobotics@menomoneefallsschools.org>
2023-01-21 14:04:27 -0600

    Camera code that doesnt import - Aiden Moore

---
+++ src/main/java/frc/robot/subsystems/LifeCam.java
@@ -0,0 +1,17 @@
+/*
+package frc.robot.subsystems;
+
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.cscore.UsbCamera;
+
+public class LifeCam extends SubsystemBase {
+  UsbCamera lifeCamArm;
+
+  public void startVision() {
+    lifeCamArm = CameraServer.startAutomaticCapture(0);
+    lifeCamArm.setResolution(160, 120);
+    lifeCamArm.setFPS(10);
+  }
+}
+*/
